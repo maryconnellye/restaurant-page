6912d47622826bf202e5a5365abfa7c7fffdb770
Mary Connelly <maryconnellye@gmail.com>
2025-02-26 09:59:08 -0600

    created new divs within content div for tab functions and fixed homepage layout

--- src/homepage.js
+++ src/homepage.js
@@ -5,18 +5,22 @@ export function homePage() {
 
 	const content = document.querySelector("#content");
 
+	const homePageContent = document.createElement('div');
+		content.appendChild(homePageContent);
+		homePageContent.id = "home-page-content";
+
 	const asher = document.createElement("img");
-	asher.src = asherPicture;
-	asher.id = "asher";
-	content.appendChild(asher);
+		homePageContent.appendChild(asher);
+		asher.src = asherPicture;
+		asher.id = "asher";
 
 	const title = document.createElement('h1');
-	content.appendChild(title);
+		homePageContent.appendChild(title);
 		title.id="title";
 		title.textContent = "Two Burritos Asher";
 
 	const about = document.createElement("p");
-		content.appendChild(about);
+		homePageContent.appendChild(about);
 		about.id = "about";
-		about.textContent = 'Asher, a long-time lover of the Mexican cuisine, once finished a whole burrito and started on a second in the time it took his wife to eat one street taco and said, "They don\'t call me "Two Burritos Asher" for nothing." Rolling her eyes, his wife pointed out nobody actually him that to which Asher gleefully explained that yes, in fact, whenever he orders at a taco truck, they yell out to him from the pick-up window, "Two burritos, Asher?"';
+		about.textContent = 'Asher, a long-time lover of the Mexican cuisine, once finished a whole burrito and started on a second in the time it took his wife to eat one street taco and said, "They don\'t call me "Two Burritos Asher" for nothing." Rolling her eyes, his wife pointed out that nobody actually calls him that.  Asher then gleefully explained that yes, in fact, whenever he orders at a taco truck, they yell out to him from the pick-up window, "Two burritos, Asher?"';
 };
